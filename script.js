a105d12023ff783b1751d9428a7e82d91e340166
Jean-Louis Guittard <jl.guittard@gmail.com>
2025-04-07 23:55:01 +0200

    v1

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const toggler = document.querySelector(".hamburger");
 const navLinksContainer = document.querySelector(".navlinks-container");
 
-const toggleNav = e => {
-  // Animation du bouton
-  toggler.classList.toggle("open");
+const toggleNav = (e) => {
+    // Animation du bouton
+    toggler.classList.toggle("open");
 
-  const ariaToggle =
-    toggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
-  toggler.setAttribute("aria-expanded", ariaToggle);
+    const ariaToggle = toggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    toggler.setAttribute("aria-expanded", ariaToggle);
 
-  // Slide de la navigation
-  navLinksContainer.classList.toggle("open");
+    // Slide de la navigation
+    navLinksContainer.classList.toggle("open");
 };
 
 toggler.addEventListener("click", toggleNav);
 
-
-new ResizeObserver(entries => {
-  if (entries[0].contentRect.width <= 900){
-    navLinksContainer.style.transition = "transform 0.3s ease-out";
-  } else {
-    navLinksContainer.style.transition = "none";
-  }
+new ResizeObserver((entries) => {
+    if (entries[0].contentRect.width <= 900) {
+        navLinksContainer.style.transition = "transform 0.3s ease-out";
+    } else {
+        navLinksContainer.style.transition = "none";
+    }
 }).observe(document.body);
