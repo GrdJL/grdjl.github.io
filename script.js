82996fccec2a8097482a90e8b4c01373c9933aa6
Jean-Louis Guittard <jl.guittard@gmail.com>
2025-04-07 22:30:14 +0200

    123

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ toggler.addEventListener("click", toggleNav);
 
 new ResizeObserver(entries => {
   if (entries[0].contentRect.width <= 900){
-    navLinksContainer.style.transition = "transform 0.4s ease-out";
+    navLinksContainer.style.transition = "transform 0.3s ease-out";
   } else {
     navLinksContainer.style.transition = "none";
   }
